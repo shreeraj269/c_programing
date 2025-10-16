e117008d552fcba5e85c76d22f808ecc7484cb36
shreeraj269 <rajpisal269@gmail.com>
2025-10-16 09:46:58 +0530

    Update task_1.c

--- task_1.c
+++ task_1.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 int main(){
 
+/*  name:shreeraj
+    UIN:251P041
+    branch:computer engineering
+    division: c
+*/
+
+    
+
 printf("name:\t\t   shreeraj pisal\n");
 printf("age:\t\t    18\n");
 printf("class:\t\t COMPUTER ENGINEERING\n");
@@ -8,3 +16,4 @@ printf("division:\t\t C\n");
 printf("UIN:\t\t 251P041\n");
     return 0;
 }
+
