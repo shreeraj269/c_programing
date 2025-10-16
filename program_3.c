a34ea408e628460c51cdd392799def1c282f74b4
shreeraj269 <rajpisal269@gmail.com>
2025-10-16 09:13:23 +0530

    Add files via upload

---
+++ program_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+           int num, sum = 0;
+         do{
+             printf("enter a number");
+         scanf("%d",&num);
+         sum += num;
+}
+       while(num !=0);
+
+
+
+    return 0;
+}
